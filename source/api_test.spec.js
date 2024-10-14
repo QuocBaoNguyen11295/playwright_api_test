8174c4edbcbee3a3dbaf1848835f73495f45aa0d
QuocBaoNguyen11295 <nqb11295@gmail.com>
2024-10-14 21:55:14 +0700

    Add some more tests

--- source/api_test.spec.js
+++ source/api_test.spec.js
@@ -17,15 +17,38 @@ test.describe('Login and access booking list', () => {
                 }
             })
             expect(res.status()).toBe(statuscode)
-            //expect(res.token).not.toBe(null)
+            expect(res.token).not.toBe(null)
         });
     })
-
 
     test('Get all booking list', async ({ request }) => {
         const res = await request.get("https://restful-booker.herokuapp.com/booking")
         expect(res.status()).toBe(200)
         const data = await res.json()
+        id = data[0].bookingid
         expect(data.length).toBeGreaterThanOrEqual(1000)
+    })
+
+    test('The user tries to filter by name but not found', async ({ request }) => {
+        const res = await request.get("https://restful-booker.herokuapp.com/booking?firstname=sally")
+        const data = await res.json()
+        expect(res.status()).toBe(200)
+        expect(data.length).toEqual(0)
+    })
+
+    test('The user tries to filter by name and found', async ({ request }) => {
+        const res = await request.get("https://restful-booker.herokuapp.com/booking?firstname=Susan")
+        const data = await res.json()
+        expect(res.status()).toBe(200)
+        expect(data.length).toBeGreaterThan(0)
+    })
+
+    test('The user tries to access the book by its id', async ({ request }) => {
+        const res = await request.get(`https://restful-booker.herokuapp.com/booking/1`)
+        const data = await res.json()
+        expect(res.status()).toBe(200)
+        expect(data.firstname).not.toBeUndefined
+        expect(data.lastname).not.toBeUndefined
+        expect(data.totalprice).not.toBeUndefined
     })
 });
