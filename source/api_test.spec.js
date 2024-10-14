93ee6023cf738896eebb922b015c42f498bd1d0c
QuocBaoNguyen11295 <nqb11295@gmail.com>
2024-10-14 20:47:22 +0700

    update tests

--- source/api_test.spec.js
+++ source/api_test.spec.js
@@ -3,9 +3,9 @@ import { test, expect } from '@playwright/test';
 test.describe('Login and access booking list', () => {
     [
         { username: 'Alice', pwd: 'admin', statuscode: 200 },
-        { username: 'Bob', pwd: 'admin2', statuscode: 200 },
-        { username: '', pwd: 'admin', statuscode: 200 },
-        { username: 'Alice', pwd: '', statuscode: 200 }
+        { username: 'Bob', pwd: 'admin2', statuscode: 403 },
+        { username: '', pwd: 'admin', statuscode: 403 },
+        { username: 'Alice', pwd: '', statuscode: 403 }
     ].forEach(({ username, pwd, statuscode }) => {
         var username_replace = username === '' ? "None" : username
         var password_replace = pwd === '' ? "None" : pwd
