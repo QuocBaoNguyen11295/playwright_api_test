e12a84d4e54f6f81213bce6933d936b09f9d7b8a
QuocBaoNguyen11295 <nqb11295@gmail.com>
2024-10-15 22:55:17 +0700

    update

--- playwright.config.js
+++ playwright.config.js
@@ -1,40 +1,23 @@
-import { defineConfig, devices } from '@playwright/test';
+const { defineConfig, devices } = require('@playwright/test');
 
-export default defineConfig({
-    // Look for test files in the "tests" directory, relative to this configuration file.
-    testDir: 'source',
-    testIgnore: '',
-
-    // Glob patterns or regular expressions that match test files.
+module.exports = defineConfig({
+    testDir: 'tests', // Ensure this points to the correct directory
     testMatch: '**/*.spec.js',
-    // Run all tests in parallel.
     fullyParallel: true,
-
-    // Fail the build on CI if you accidentally left test.only in the source code.
     forbidOnly: !!process.env.CI,
-
-    // Retry on CI only.
     retries: process.env.CI ? 2 : 0,
-
-    // Opt out of parallel tests on CI.
     workers: process.env.CI ? 1 : undefined,
-
-    // Reporter to use
     reporter: 'html',
 
     use: {
-        // Base URL to use in actions like `await page.goto('/')`.
         baseURL: 'http://127.0.0.1:3000',
-
-        // Collect trace when retrying the failed test.
         trace: 'on-first-retry',
     },
-    // Configure projects for major browsers.
+
     projects: [
         {
             name: 'chromium',
             use: { ...devices['Desktop Chrome'] },
         },
     ],
-    // Run your local dev server before starting the tests.
 });
